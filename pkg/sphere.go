214a3ed3baf1f5651d113fb43ee61e5c8e34f4fa
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-10-06 22:44:10 -0400

    Surface Normals

--- pkg/sphere.go
+++ pkg/sphere.go
@@ -17,3 +17,11 @@ func NewSphere() Sphere {
 func (s *Sphere) SetTransform(m Matrix) {
 	s.Transform = m
 }
+
+func (s Sphere) NormalAt(p Point) Vector {
+	objectPoint := s.Transform.Inverse().Mult(p)
+	objectNormal := objectPoint.Sub(NewPoint(0, 0, 0))
+	worldNormal := s.Transform.Inverse().Transpose().Mult(objectNormal)
+	worldNormal.w = 0.0
+	return worldNormal.Normalize()
+}
--- pkg/sphere_test.go
+++ pkg/sphere_test.go
@@ -1,6 +1,7 @@
 package goray
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,5 +19,43 @@ func TestSphereTransform(t *testing.T) {
 		s.SetTransform(transform)
 
 		assert.True(t, MatricesEqual(s.Transform, transform))
+	})
+}
+
+func TestSurfaceNormal(t *testing.T) {
+	s := NewSphere()
+
+	sqrt33 := math.Sqrt(3) / 3.0
+
+	testCases := map[Point]Vector{
+		NewPoint(1, 0, 0):                NewVector(1, 0, 0).Normalize(),
+		NewPoint(0, 1, 0):                NewVector(0, 1, 0).Normalize(),
+		NewPoint(0, 0, 1):                NewVector(0, 0, 1).Normalize(),
+		NewPoint(sqrt33, sqrt33, sqrt33): NewVector(sqrt33, sqrt33, sqrt33).Normalize(),
+	}
+
+	for p, n := range testCases {
+		assert.True(t, TuplesEqual(s.NormalAt(p), n))
+	}
+}
+
+func TestNormalOnTransformedSphere(t *testing.T) {
+	t.Run("on a translated sphere", func(t *testing.T) {
+		s := NewSphere()
+		s.SetTransform(Translation(0, 1, 0))
+
+		n := s.NormalAt(NewPoint(0, 1.70711, -0.70711))
+
+		assert.True(t, TuplesEqual(n, NewVector(0, 0.70711, -0.70711)))
+	})
+
+	t.Run("on a transformed sphere", func(t *testing.T) {
+		s := NewSphere()
+		m := Scaling(1, 0.5, 1).Mul(RotationZ(math.Pi / 5))
+		s.SetTransform(m)
+
+		n := s.NormalAt(NewPoint(0, math.Sqrt2/2, -math.Sqrt2/2))
+
+		assert.True(t, TuplesEqual(n, NewVector(0, 0.97014, -0.24254)))
 	})
 }
